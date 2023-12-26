74330ea33fb545464dc847c42f03132dff173b36
mateus <mhlbueno@hotmail.com>
2023-12-26 08:36:59 -0300

    Aula de pilares da POO pt.2

--- C#/ExemploPOO/Program.cs
+++ C#/ExemploPOO/Program.cs
@@ -1,15 +1,40 @@
 ﻿using ExemploPOO.Models;
 
-ContaCorrente ContaP1 = new ContaCorrente("12345-1", 300);
+
+
+Aluno a1 = new Aluno();
+a1.Nome = "Bueno";
+a1.Idade = 19;
+a1.Email = "generico@teste.com";
+a1.Nota = 9;
+a1.Apresentar();
+
+Professor p1 = new Professor();
+p1.Nome = "Leonardo";
+p1.Idade = 20;
+p1.Salario = 2000;
+p1.Apresentar();
 
 
 
 
-ContaP1.Depositar(200);
-ContaP1.VerSaldo();
-ContaP1.Sacar(400);
-ContaP1.VerSaldo();
-ContaP1.Sacar(200);
+
+
+
+
+
+
+
+
+
+
+//ContaCorrente ContaP1 = new ContaCorrente("12345-1", 300);
+
+// ContaP1.Depositar(200);
+// ContaP1.VerSaldo();
+// ContaP1.Sacar(400);
+// ContaP1.VerSaldo();
+// ContaP1.Sacar(200);
 
 
 
