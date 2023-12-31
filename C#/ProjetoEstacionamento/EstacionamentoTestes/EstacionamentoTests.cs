afb0aeb8a085fe94e45ab81060d536869b8c2de4
mateus <mhlbueno@hotmail.com>
2023-12-30 23:29:55 -0300

    finalizando o projeto estacionamento

--- C#/ProjetoEstacionamento/EstacionamentoTestes/EstacionamentoTests.cs
+++ C#/ProjetoEstacionamento/EstacionamentoTestes/EstacionamentoTests.cs
@@ -4,34 +4,5 @@ namespace EstacionamentoTestes;
 public class EstacionamentoTests
 {
 
-    private EstacionamentoImp _es;
-
-    [Fact]
-    public void IncluirPlacaPadraoMercosul()
-    {
-        //Arrange
-        string placa = "AAA1234";
-
-        //Act
-        
-
-
-        //Assert
-    }
-
-    [Fact]
-    public void IncluirPlacaPadraoNormal()
-    {
-        //Arrange
-        //Act
-        //Assert
-    }
-
-    [Fact]
-    public void IncluirPlacaNoFormatoErrado()
-    {
-        //Arrange
-        //Act
-        //Assert
-    }
+    //private EstacionamentoImp _es;
 }
